2931754247bff9478deafcffb210767740790f2c
Tri Aribowo <t.rahmat.aribowo@gmail.com>
2020-01-27 23:25:54 +0700

    s

---
+++ models/order-item.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../until/database');
+const OrderItem = sequelize.define('orderItem', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    quantity: Sequelize.INTEGER
+});
+
+module.exports = OrderItem;
---
+++ models/order.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../until/database');
+const Order = sequelize.define('order', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Order;
